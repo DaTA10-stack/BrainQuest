3e6d257032074a4d15dc2f1b4db6857da19e6e7f
DaTA10-stack <dhruv.anantha10@gmail.com>
2026-03-14 14:43:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+const supabase = supabase.createClient('https://indsorfjcozlnpgijamo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZHNvcmZqY296bG5wZ2lqYW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0OTk1MjAsImV4cCI6MjA4OTA3NTUyMH0.upc8ooD0ndrWnQV0qnCyO04hMgBqTAepYAud_52OoBU');
 
-const supabaseUrl = "https://indsorfjcozlnpgijamo.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZHNvcmZqY296bG5wZ2lqYW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0OTk1MjAsImV4cCI6MjA4OTA3NTUyMH0.upc8ooD0ndrWnQV0qnCyO04hMgBqTAepYAud_52OoBU"
-
-const supabase = createClient(supabaseUrl, supabaseKey)
 
 import { saveScore } from "./scores.js"
 
